80cdaac084834ed442e82b2ab3c4f05f43f8bb1f
Konstantin <kv_psk@mail.ru>
2022-09-05 15:56:54 +0300

    Added Second Var of Solution

--- Task_Solution/Program.cs
+++ Task_Solution/Program.cs
@@ -3,14 +3,12 @@
 // длина которых меньше либо равна 3 символа.                                =
 //============================================================================
 
-
+Console.Clear();
 //--------------------------------
 // Первый вариант решения задачи 
 //--------------------------------
 
 //Получаем от пользователя данные о размере массива и просим ввести значение каждого элемента
-Console.Clear();
-
 string[] UserArray()
 {
     Console.ForegroundColor = ConsoleColor.Red;
@@ -34,6 +32,7 @@ string[] UserArray()
 //Метод нахождения символов в массиве с длиной меньше либо равной 3
 string[] SearchLength(string[] inputArray)
 {
+
     string[] outputArray = new string[0];
     int m = 0;
     for (int i = 0; i < inputArray.Length; i++)
@@ -56,12 +55,16 @@ string[] SearchLength(string[] inputArray)
     {
         Console.WriteLine("Элементы которые соответствуют заданному условию:");
         return (outputArray);
+
     }
+
+
 }
 
 
-//Вывод результата на экран
-void PrintColourStringArray(string[] inputArray)
+
+//Метод для вывода результата на экран
+void PrintFirstStringArray(string[] inputArray)
 {
     Console.ForegroundColor = ConsoleColor.Red;
     if (inputArray.Length == 0)
@@ -85,10 +88,69 @@ void PrintColourStringArray(string[] inputArray)
 
 
 string[] secondBufferArray = UserArray();
-PrintColourStringArray(SearchLength(secondBufferArray));
+PrintFirstStringArray(SearchLength(secondBufferArray));
 
 
 
+//--------------------------------
+// Второй вариант решения задачи 
+//--------------------------------
+
+//Получаем размер массива с консоли и заполняем случайными символами
+
+string[] FillingArray()
+{
 
 
+    int m = 0;
+    Console.WriteLine("Введите количество элементов массива:");
+    m = int.Parse(Console.ReadLine() ?? "");
+    string[] ReadNumber = new string[m];
 
+    Random rnd = new Random();
+    for (int i = 0; i < ReadNumber.Length; i++)
+    {
+        int stringLength = rnd.Next(1, 6);
+        int randNumber;
+        char symbol;
+        string str = "";
+        for (int j = 0; j < stringLength; j++)
+        {
+            randNumber = rnd.Next(0, 92);
+            symbol = Convert.ToChar(randNumber + 33);
+            str = str + symbol;
+
+        }
+        ReadNumber[i] = str;
+    }
+
+    return (ReadNumber);
+}
+
+void PrintSecondStringArray(string[] inputArray)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    if (inputArray.Length == 0)
+    {
+        Console.WriteLine("Элементов соответствующих заданию не обнаружено");
+    }
+    else
+    {
+        int i = 0;
+        while (i < inputArray.Length - 1)
+        {
+            Console.Write(inputArray[i] + ", ");
+            i++;
+        }
+
+        Console.WriteLine(inputArray[i]);
+    }
+    Console.ResetColor();
+}
+
+
+string[] bufferArray = FillingArray();
+//Выводим на экран сгенерированный массив
+PrintSecondStringArray(bufferArray);
+//Ищем элементы
+PrintSecondStringArray(SearchLength(bufferArray));
